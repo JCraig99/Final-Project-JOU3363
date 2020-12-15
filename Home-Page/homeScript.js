52ddac3669913ac82dd0a11f7b32c4a16f220f99
JCraig99 <71850784+JCraig99@users.noreply.github.com>
2020-12-14 20:02:21 -0500

    Version 1.0 Done

---
+++ Home-Page/homeScript.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+
+/*
+const navSlide = () => {
+  const container = document.querySelector('.container');
+  const nav = document.querySelector('li');
+
+  container.addEventListener ('click', () => {
+    nav.classList.toggle ('li');
+  });
+};
+
+ navSlide();
+
+ */
